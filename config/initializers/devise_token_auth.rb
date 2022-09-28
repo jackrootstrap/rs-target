8f0616db004e3ee54281818079e26af605819c7b
Jack Ibarra <jack.ibarra@rootstrap.com>
2022-09-28 12:49:44 -0500

    Add specs for confirmable feature

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -3,5 +3,5 @@
 DeviseTokenAuth.setup do |config|
   config.change_headers_on_each_request = false
   config.token_cost = Rails.env.test? ? 4 : 10
-  config.default_confirm_success_url = '/confirm_email'
+  config.default_confirm_success_url = 'http://localhost:3000'
 end
