803e9a2cca7e8f712c21de56f3c48d5cc479d54f
Jack Ibarra <jack.ibarra@rootstrap.com>
2022-09-30 15:45:53 -0500

    Update api controller

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -3,8 +3,6 @@
 module Api
   module V1
     class TopicsController < ApiController
-      before_action :authenticate_user!
-
       def index
         @topics = Topic.all
       end
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,4 +2,5 @@
 
 class ApiController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :authenticate_user!
 end
